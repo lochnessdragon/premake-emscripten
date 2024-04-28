4058c192c716aa70a8f6eca9fcd6fab0e646b7d0
Joao Matos <joao@tritao.eu>
2024-04-28 20:04:36 +0100

    Preload _preload.lua.

--- emscripten.lua
+++ emscripten.lua
@@ -10,6 +10,9 @@
 	local m = p.modules.emscripten
 	m._VERSION = "0.0.1"
 
+	-- remove this if you want to embed the module
+	dofile "_preload.lua"
+
 	if os.locate("premake-vstool/vstool.lua") or os.locate("vstool/vstool.lua") then
 		require "vstool"
 	end
